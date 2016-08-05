3fa14aeb2021e3f5d8f1e12b33ea8bb54e7f4648
김태완 <Louis@gimtaewan-ui-MacBook-Pro.local>
2016-08-05 21:55:16 +0900

    log in 보완

--- blocus/db/migrate/20160804111810_devise_create_users.rb
+++ blocus/db/migrate/20160804111810_devise_create_users.rb
@@ -1,6 +1,10 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+
+      ## customize
+      t.string :name
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- blocus/db/schema.rb
+++ blocus/db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160805082409) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
