e1348eba528aabc734b384f5e145eadef33c26ad
jaeseong <ljs8860@snu.ac.kr>
2016-08-05 15:15:01 +0900

    resource region

--- blocus/app/controllers/regions_controller.rb
+++ blocus/app/controllers/regions_controller.rb
@@ -1,11 +1,43 @@
 class RegionsController < ApplicationController
+
   def index
-    @region=Region.find(params[:region_id])
+    @regions=Region.all
   end
+
+  def new
+    @region = Region.new
+  end
+
   def create
-
+    @region = Region.create(region_params)
+    
+    redirect_to regions_path
   end
+
   def show
-
+    @region = Region.find(params[:id])
   end
+
+  def edit
+    @region = Region.find(params[:id])
+  end
+
+  def update
+    @region = Region.find(params[:id])
+    @region.update(region_params)
+
+    redirect_to region_path(@region)
+  end
+
+  def destroy
+    @region = Region.find(params[:id])
+    @region.destroy
+    redirect_to regions_path
+  end
+  
+  private
+  def region_params
+    params.require(:region).permit(:name)
+  end
+
 end
